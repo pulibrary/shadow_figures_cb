04ff51c7f7cc4b3f74e8bc9fdd9a35f70677fdb9
Cliff Wulfman <cwulfman@princeton.edu>
2023-08-31 22:23:50 -0400

    working version

--- utilities/metadata_pages.py
+++ utilities/metadata_pages.py
@@ -1,58 +1,117 @@
-"""Tools for processing source csv into usable metadata files
+"""Generate metadata csv for CollectionBuilder from external datafile.
+
+The raw data is in a tsv file
 """
 import re
 from csv import DictReader, DictWriter
 from pathlib import Path
+import urllib.parse
+import os
+from functools import partial
+
 
 source = "../_data/shadowfigures.tsv"
-imagedir = Path("../objects")
+# imagedir = Path("../objects")
 
-images = []
+bucket = Path("ealshadowobjects")
+
+def identifier_from_image_name(image_name):
+    return urllib.parse.quote_plus(f"{bucket}/{os.path.splitext(image_name)[0]}")
+
+def iiif_uri(scheme, server, prefix, identifier, region, size, rotation, quality, format):
+    return f"{scheme}://{server}/{prefix}/{identifier}/{region}/{size}/{rotation}/{quality}.{format}"
+
+object_uri = partial(iiif_uri,
+              scheme="https",
+              server="puliiif-staging.princeton.edu",
+              prefix="iiif/2",
+              region="full",
+              rotation=0,
+              quality="default",
+              format="jpg")
+
+def object_location(image_name):
+    return object_uri(identifier=identifier_from_image_name(image_name),
+                      size="full")
+
+def image_small(image_name):
+    return object_uri(identifier=identifier_from_image_name(image_name),
+                      size="800,")
+
+def image_thumb(image_name):
+    return object_uri(identifier=identifier_from_image_name(image_name),
+                      size="400,")
+
+
+def extant_images(names_string, flags_string):
+    images = names_string.split('|')
+    flags = flags_string.split('|')
+    if len(images) != len(flags):
+        raise(ValueError("image list and flag list are not the same size"))
+    return [fname for fname, flag in zip(images, flags) if flag == 'YES']
+
+def object_title(description):
+    pattern = re.compile(r"^([^\.]+)")
+    try:
+        title = pattern.match(description)[0]
+    except IndexError:
+        result = "Untitled"
+    return title
+
+
 objects = []
 with open(source, mode="r", encoding="utf-8") as f:
     reader = DictReader(f, delimiter='\t')
     for row in reader:
         object = {}
-        objectid = f"object_{row['objectno'].rjust(3, '0')}"
+        objectid = f"object_{row['objectno'].rjust(4, '0')}"
         object['objectid'] = objectid
-        object['title'] = row['objecttype']
+        object['identifier'] = row['objectno']
+        object['title'] = object_title(row['description'])
         object['subject'] = row['objecttype']
+        object['dimensions'] = row['dimensions']
         object['display_template'] = 'record'
         description = re.sub(r"\n", " ", row['description'])
         object['description'] = description
         object['notes'] = row['notes']
         # objects.append(object)
         images = []
-        for i,imagename in enumerate(row['imagename'].split('|')):
-            filename = Path(imagename.lower())
-            if (imagedir / filename).is_file():
-                
-                image = {}
-                image['objectid'] = f"{objectid}_{i}"
-                image['parentid'] = objectid
-                image['title'] = f"{str(filename)} containing {objectid}"
-                image['display_template'] = 'image'
-                image['format'] = f"image/{filename.suffix[1:]}"
-                image['object_location'] = f"/objects/{filename}"
-                image['image_small'] = f"/objects/small/{filename.stem}_sm{filename.suffix}"
-                image['image_thumb'] = f"/objects/thumbs/{filename.stem}_th{filename.suffix}"
-                image['image_alt_text'] = f"image containing {objectid}"
-                images.append(image)
-                if len(images) == 1:
-                    object['display_template'] = 'item'
-                    object['image_thumb'] = images[0]['image_thumb']
+        real_images = extant_images(row['imagename'], row['imageexists'])
+        try:
+            thumbnail = image_thumb(real_images[0])
+        except IndexError:
+            thumbnail = None
+            print(f"no images for {objectid}")
 
-                elif len(images) > 1:
-                    object['display_template'] = 'compound_object'
-                    object['image_thumb'] = images[0]['image_thumb']
+        if len(real_images) == 1:
+            object['display_template'] = 'item'
+        else:
+            object['display_template'] = 'compound_object'
+        object['image_thumb'] = thumbnail
+
+        for i,image_name in enumerate(real_images):
+            image = {}
+            image['objectid'] = f"{objectid}_{i}"
+            image['parentid'] = objectid
+            filename = Path(image_name)
+            image['title'] = f"{str(filename)} containing {objectid}"
+            image['display_template'] = 'image'
+            image['format'] = "image/jpg" # as delivered by IIIF server
+            image['object_location'] = object_location(image_name)
+            image['image_small'] = image_small(image_name)
+            image['image_thumb'] = image_thumb(image_name)
+            image['image_alt_text'] = f"image containing {objectid}"
+            images.append(image)
+
         objects.append(object)
         objects = objects + images
-                
 
 
-with open("../_data/metadata.csv", "w", newline='', encoding="utf-8") as out:
-    writer = DictWriter(out, fieldnames=['objectid', 'parentid', 'title','subject','display_template','format','object_location','image_small','image_thumb','image_alt_text','description', 'notes'])
+
+outfile = "../_data/metadata.csv"
+#outfile = "/tmp/cb_test.csv"
+with open(outfile, "w", newline='', encoding="utf-8") as out:
+    writer = DictWriter(out, fieldnames=['objectid', 'parentid', 'title','identifier','subject','dimensions','display_template','format','object_location','image_small','image_thumb','image_alt_text','description', 'notes'])
     writer.writeheader()
     for object in objects:
         writer.writerow(object)
-        
